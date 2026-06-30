19e15170b25d629083cf1d0a483eefcdbccb2806
Daniel Biasiotto <mail@denial.in.net>
2026-06-30 11:11:34 +0200

    build: add pyproject.toml for proper package install (ISS-026)

--- scripts/visualize_physics.py
+++ scripts/visualize_physics.py
@@ -1,7 +1,3 @@
-import os
-import sys
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
 import numpy as np
 from scipy.spatial.transform import Rotation as R
 import pyray as pr
